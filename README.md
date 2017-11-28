c0b7b7e6b48631b2289a205c4dac6159e0b6f43e
Niall Coady <niall_coady7@hotmail.com>
2017-11-28 09:56:36 +0000

    Initial commit, ability to donate to a candidate, view donation report and add a candidate

---
+++ README.md
@@ -0,0 +1,1 @@
+# Aurelia-donation-webapp
---
+++ src/viewmodels/candidates.js
@@ -0,0 +1,21 @@
+import {inject} from 'aurelia-framework';
+import DonationService from '../services/donation-service';
+
+@inject(DonationService)
+export class Candidate {
+
+  firstName = '';
+  lastName = '';
+  office = '';
+
+  constructor(ds) {
+    this.donationService = ds;
+    this.firstName = ds.firstName;
+    this.lastName = ds.lastName;
+    this.office = ds.office;
+  }
+
+  addCandidate() {
+    this.donationService.addCandidate(this.firstName, this.lastName, this.office);
+  }
+}
